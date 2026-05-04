8d06a292b8029020cdcc236c14f89bf69422967c
allentertainmentsstudio <allentertainmentsstudio@gmail.com>
2026-05-04 11:20:10 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,20 @@
+from pymongo import MongoClient
+from config import MONGO_URI, DB_NAME
+
+client = MongoClient(MONGO_URI)
+db = client[DB_NAME]
+
+users = db.users
+
+
+def add_user(user_id, name):
+    if not users.find_one({"user_id": user_id}):
+        users.insert_one({"user_id": user_id, "name": name, "files": 0})
+
+
+def inc_files(user_id):
+    users.update_one({"user_id": user_id}, {"$inc": {"files": 1}})
+
+
+def get_user_count():
+    return users.count_documents({})
