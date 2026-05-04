f10e28f057ab8e5dc7471bec87985300c0f69b08
allentertainmentsstudio <allentertainmentsstudio@gmail.com>
2026-05-04 11:26:53 +0530

    Update database.py

--- database.py
+++ database.py
@@ -5,8 +5,10 @@ client = MongoClient(MONGO_URI)
 db = client[DB_NAME]
 
 users = db.users
+banned = db.banned
 
 
+# ---------- USERS ---------- #
 def add_user(user_id, name):
     if not users.find_one({"user_id": user_id}):
         users.insert_one({"user_id": user_id, "name": name, "files": 0})
@@ -18,3 +20,17 @@ def inc_files(user_id):
 
 def get_user_count():
     return users.count_documents({})
+
+
+# ---------- BAN SYSTEM ---------- #
+def ban_user(user_id):
+    if not banned.find_one({"user_id": user_id}):
+        banned.insert_one({"user_id": user_id})
+
+
+def unban_user(user_id):
+    banned.delete_one({"user_id": user_id})
+
+
+def is_banned(user_id):
+    return banned.find_one({"user_id": user_id}) is not None
